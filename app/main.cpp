34c4746ec46974ac62c73296b30266a57075969c
Sayan Brahma <sbrahma0@gmail.com>
2019-09-25 21:26:02 -0400

    code update

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,35 @@
-#include <iostream>
-#include <lib.hpp>
+/*!
+ *  \brief 		File including the class definition and stub
+ *  \file  		Header file - pid_controller
+ *  \author 	part1: Sayan Brahma(driver) Akwashi a Obeng(navigator)
+ *  \author 	part2: Akwashi a Obeng(driver) Sayan Brahma(navigator)
+ *  \copyright  Sayan Brahma and Akwashi a Obeng
+ *  \
+ */
 
-int main()
-{
-    dummy();
+#include <iostream>
+#include "../include/pid_controller.hpp"
+
+int main() {
+    double kp, ki, kd, dt, setValue, currentValue;
+    std::cout << "Enter the kp, ki, kd parameters with spaces in "
+"between- "<< std::endl;
+    std::cin >>  kp >> ki >> kd;
+    std::cout << "Enter the time in ( non-negative)- " << std::endl;
+    std::cin >> dt;
+    std::cout << "Enter the set value- " << std::endl;
+    std::cin >> setValue;
+    std::cout << "Enter the current value for trial- " << std::endl;
+    std::cin >> currentValue;
+
+
+    pidController pid(kp, ki, kd, dt);
+    double error = currentValue - setValue;
+    double cv = (kp * error) + (ki * error * dt)
+    + (kd * error / dt);
+    std::cout << "new- " << cv << std::endl;
+    double nv = pid.compute(setValue, currentValue);
+    std::cout << "The new value is- " << nv << std::endl;
     return 0;
 }
+
---
+++ app/pidController.cpp
@@ -0,0 +1,63 @@
+/*!
+ *  \brief 		File including the class definition and stub
+ *  \file  		Header file - pid_controller
+ *  \author 	part1: Sayan Brahma(driver) Akwashi a Obeng(navigator)
+ *  \author 	part2: Akwashi a Obeng(driver) Sayan Brahma(navigator)
+ *  \copyright  Sayan Brahma and Akwashi a Obeng
+ *  \
+ */
+
+#include <iostream>
+#include "../include/pid_controller.hpp"
+/**
+ * @brief constructor definition for initializing the default values
+ */
+pidController::pidController() {
+}
+/**
+ * @brief constructor definition for initializing the user input values
+ * @param kpInput user input of the kp parameter
+ * @param kiInput user input of the ki parameter
+ * @param kdInput user input of the kd parameter
+ * @param dtInput user input of the dt parameter
+ */
+pidController::pidController(double kpInput, double kiInput,
+double kdInput, double dtInput) {
+    double kp = kpInput, ki = kiInput, kd = kdInput, dt = dtInput;
+
+    std::cout << "The user entered parameters are: kp = " << kp << " ki = "
+<< ki << " kd = " << kd << " dt = " << dt << std::endl;
+}
+/**
+ * @brief The compute function definition computes the error between the reference value and the current value
+ * @param setValue This is the reference value
+ * @param currentValue This is the current value which keeps on changing
+ * @return the calculated value from the error
+ */
+double pidController::compute(double setValue, double currentValue) {
+    double error = setValue - currentValue;  // compute the error
+    std::cout << "Error- : " << error << std::endl;
+
+    double cv;
+      if (dt > 0) {
+          cv = (kp * error) + (ki * error * dt)
++ (kd * error / dt);
+          std::cout << "New value is- " << cv << std::endl;
+      } else {
+      std::cout << "Time has to be a non-negative value. "
+              "Output velocity set to 0.";
+      }
+      return cv;
+}
+/**
+ * @brief This function returns the dt value
+ * @return dt
+ */
+double pidController::returndt() {
+    return dt;
+}
+/**
+ * @brief Destructor
+ */
+pidController::~pidController() {
+}
